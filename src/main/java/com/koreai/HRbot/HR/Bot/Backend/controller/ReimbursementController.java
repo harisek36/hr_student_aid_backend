b5cc012f7743bf544ae184f011bf0b7f6bed27da
HarishSekar <hasekar@ciber.com>
2019-03-15 11:35:48 -0400

    CORS update

--- src/main/java/com/koreai/HRbot/HR/Bot/Backend/controller/ReimbursementController.java
+++ src/main/java/com/koreai/HRbot/HR/Bot/Backend/controller/ReimbursementController.java
@@ -6,17 +6,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.koreai.HRbot.HR.Bot.Backend.entity.Reimbursement;
 import com.koreai.HRbot.HR.Bot.Backend.service.ReimbursementService;
 
+@CrossOrigin(origins = {"*"},  methods= {RequestMethod.GET, RequestMethod.POST},maxAge = 3600)
 @RestController
 @RequestMapping("reimbursement")
 public class ReimbursementController {
