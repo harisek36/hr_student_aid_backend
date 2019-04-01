5cd4ccb167897389aad1d6d02d82fc6a0daf2789
HarishSekar <hasekar@ciber.com>
2019-04-01 16:40:59 -0400

    Student login/ logout Feature

---
+++ src/main/java/com/koreai/HRbot/HR/Bot/Backend/controller/CurrentStudent.java
@@ -0,0 +1,49 @@
+package com.koreai.HRbot.HR.Bot.Backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.koreai.HRbot.HR.Bot.Backend.entity.Student;
+import com.koreai.HRbot.HR.Bot.Backend.entity.StudentLoginStatus;
+
+@CrossOrigin(origins = { "*" }, methods = { RequestMethod.GET, RequestMethod.POST }, maxAge = 3600)
+@RestController
+@RequestMapping("currentStudent")
+public class CurrentStudent {
+
+	StudentLoginStatus currentStudentStatus = new StudentLoginStatus();
+
+	@GetMapping
+	StudentLoginStatus getCurrentStudent() {
+
+		return currentStudentStatus;
+	}
+
+	@PostMapping("login")
+	StudentLoginStatus signInStudent(@RequestBody Student student) {
+
+		if (student != null && currentStudentStatus.getStudent() == null) {
+
+			this.currentStudentStatus.setStudent(student);
+			this.currentStudentStatus.setLoggedIn(true);
+		} else if (currentStudentStatus.getStudent() != null) {
+			return this.currentStudentStatus;
+		}
+
+		return this.currentStudentStatus;
+	}
+
+	@GetMapping("logout")
+	StudentLoginStatus logOutStudent() {
+
+		this.currentStudentStatus.setLoggedIn(false);
+		this.currentStudentStatus.setStudent(null);
+		return this.currentStudentStatus;
+	}
+
+}
